8ee56c272c48bb89348ba13415c04044bcec35b4
nutann <nutnaik@gmail.com>
2017-04-05 17:12:07 -0700

    testing support

---
+++ public/javascripts/mainCtrl.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nutan on 4/5/2017.
+ */
+describe('MainCtrl Test', function() {
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect(2+2).toEqual(4);
+    });
+});
